459f6cd3998156b978974dae632687dbc4fa9560
Tiago Madeira <tmadeira@gmail.com>
2021-11-04 21:35:10 -0300

    Remove Navbar from _app.tsx (use Navbar from Plasmic instead)

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -104,7 +104,6 @@ const Layout: FC<Props> = ({
   return (
     <CommerceProvider locale={locale}>
       <div className={cn(s.root)}>
-        <Navbar links={navBarlinks} />
         <main className="fit">{children}</main>
         <Footer pages={pageProps.pages} />
         <ModalUI />
